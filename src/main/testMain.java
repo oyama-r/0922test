e9c9d1d8e308762aa9f8401e775f96c4966e0583
小山 竜太郎 <travel_sand@me.com>
2013-09-22 14:45:40 +0900

    initialCommitForTest
    test

---
+++ src/main/testMain.java
@@ -0,0 +1,15 @@
+package main;
+
+public class testMain {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		private static final String AAAA = "";
+
+		for (int i = 0; i > 100; i++) {
+
+		}
+
+	}
+
+}
